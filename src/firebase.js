58912d4202e7e0f3bab8f049a7dd72c82a0c757f
khaled <khaledsaouthi@gmail.com>
2025-05-27 09:07:38 -0700

    setup Firebase config with Firestore and Auth

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtifM-67J2k3v1bG98onxLvd8bgflsA3Q",
+  authDomain: "examen-fc768.firebaseapp.com",
+  projectId: "examen-fc768",
+  storageBucket: "examen-fc768.firebasestorage.app",
+  messagingSenderId: "712504490692",
+  appId: "1:712504490692:web:c954c01448e4d728fde791",
+  measurementId: "G-EQQ7TPL8ES"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
